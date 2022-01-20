f167118e984e636849f38a3d5825fe2eb1ca17d1
matt <30198937+6mp@users.noreply.github.com>
2022-01-19 18:13:27 -0800

    Delete cmake-build-debug-mingw directory

--- cmake-build-debug-mingw/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Matt/Desktop/CircularQueue
-# Build directory: C:/Users/Matt/Desktop/CircularQueue/cmake-build-debug-mingw
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
--- cmake-build-debug-mingw/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Matt/Desktop/CircularQueue/test
-# Build directory: C:/Users/Matt/Desktop/CircularQueue/cmake-build-debug-mingw/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/Matt/Desktop/CircularQueue/cmake-build-debug-mingw/test/Tests_include-b12d07c.cmake")
-subdirs("../_deps/catch2-build")
